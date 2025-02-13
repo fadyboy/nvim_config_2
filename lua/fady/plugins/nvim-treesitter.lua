84af9272fbb20ad103e676b7340639425dc0494f
fadyboy <aofadero@gmail.com>
2025-02-12 22:18:59 -0500

    removed commented code

--- lua/fady/plugins/nvim-treesitter.lua
+++ lua/fady/plugins/nvim-treesitter.lua
@@ -59,13 +59,6 @@ return {
 			},
 		})
 		require('nvim-ts-autotag').setup()
-		-- require("nvim-ts-autotag").setup({
-		-- 	opts = {
-		-- 		enable_close = true,
-		-- 		enable_rename = true,
-		-- 		enable_close_on_slash = false,
-		-- 	},
-		-- })
 	end,
 }
 
